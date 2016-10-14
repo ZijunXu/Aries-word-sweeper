8e0a7d8a04fb96e8a46c980a0d0dc91577c39879
ZijunXu <weizhentiantian@gmail.com>
2016-10-14 17:15:33 -0400

    change the name of the class
    seems that the name of the class would not change during the push

---
+++ src/ClientLauncher.java
@@ -0,0 +1,8 @@
+import client.view.GameModePanel;
+
+public class ClientLauncher {
+	public static void main(String [] args){
+		GameModePanel frame = new GameModePanel();
+		frame.setVisible(true);
+	}	
+}
