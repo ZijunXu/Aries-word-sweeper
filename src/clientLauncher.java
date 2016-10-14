5637e6b5828a3ae3a9037564744421894dd4f16e
ZijunXu <weizhentiantian@gmail.com>
2016-10-14 17:16:10 -0400

    Delete clientLauncher.java

--- src/clientLauncher.java
+++
@@ -1,8 +0,0 @@
-import client.view.GameModePanel;
-
-public class ClientLauncher {
-	public static void main(String [] args){
-		GameModePanel frame = new GameModePanel();
-		frame.setVisible(true);
-	}	
-}
